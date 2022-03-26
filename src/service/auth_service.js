6b0fb06640cd32bbb26992936a2b33d328168909
Geonmin Park <wianw@naver.com>
2022-03-27 08:02:44 +0900

    project set up and firebase

---
+++ src/service/auth_service.js
@@ -0,0 +1,12 @@
+import { FirebaseError } from 'firebase/app';
+import firebase from 'firebase/compat';
+
+class AuthService{
+
+    login(providerName){
+        const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+        return firebase.auth().signInWithPopup(authProvider);
+    }
+}
+
+export default AuthService;
